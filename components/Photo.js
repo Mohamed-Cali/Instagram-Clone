c885d4ad8dca3bdfeb7c3ebcb2326e765278fbdf
Mohamed Cali <mohamedcali@Mohameds-Air.cust.communityfibre.co.uk>
2022-12-05 17:55:16 +0000

    ready to deploy

--- components/Photo.js
+++ components/Photo.js
@@ -4,12 +4,6 @@ import React from 'react'
 function Photo({ id, username, userImg, img, caption }) {
     const { data: session } = useSession();
 
-
-  
-
-  
-
-
     return (
         <div className="bg-white my-7 border rounded-sm">
            
